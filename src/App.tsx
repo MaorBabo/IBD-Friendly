ce3c6b7439a4b27b6d6f59eb953382fafa3b61f7
Maor Babo <maor.babo123@gmail.com>
2023-11-10 11:15:08 +0200

    adding a simple get request to the app component

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,39 @@ import SignInOut from "./pages/SignInOut";
 import RecipeGenerator from "./pages/RecipeGenerator";
 import NoPage from "./pages/NoPage";
 import Contact from "./pages/Contact";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  email: string;
+  name: string;
+  password: string;
+  whiteList: string[];
+  blackList: string[];
+  recipeList: string[];
+}
 
 function App() {
   let whiteList = ["avocado", "eggs", "peanut butter"];
   let blackList = ["tomato", "coffee", "processed meat"];
   let recipeList = ["link1", "link2", "link3"];
+
+  const [user, setUser] = useState<User>({
+    email: "",
+    name: "",
+    password: "",
+    whiteList: [],
+    blackList: [],
+    recipeList: [],
+  });
+
+  useEffect(() => {
+    axios
+      .get<User>({
+        /* paste backend url here*/
+      })
+      .then((res) => setUser(res));
+  }, []);
 
   return (
     <BrowserRouter>
